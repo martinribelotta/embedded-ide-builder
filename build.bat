7b67d68b20d44fd04b1d2b121abc040e73a20865
Martin Ribelotta <martinribelotta@gmail.com>
2016-09-11 18:03:20 -0300

    Download and generate base
    Signed-off-by: Martin Ribelotta <martinribelotta@gmail.com>

--- build.bat
+++ build.bat
@@ -1,1 +1,14 @@
-%~dp0/busybox.exe sh %~dp0/build.sh
+set PATH=%~dp0\bin
+
+mingw-get update
+
+set packages=
+SetLocal EnableDelayedExpansion
+for /F "delims=" %%i in (packages.txt) do set packages=!packages! %%i
+
+echo Installing %packages%
+mingw-get install %packages%
+EndLocal
+
+:1
+pause
